17b1ddc6f7d8081242c778f65e5949f6b5383d2d
mofodox <khairulakmal.dev@gmail.com>
2025-05-07 21:36:12 +0800

    chore: fix navbar brand size

--- app/components/Logo.tsx
+++ app/components/Logo.tsx
@@ -5,13 +5,17 @@ import Image from 'next/image';
 
 export function Logo() {
   return (
-    <Link href="/" className="flex items-center space-x-2">
-      <Image 
-        src='/images/logo.png'
-        alt='EarlySteps Logo'
-        width={72}
-        height={72}
-      />
+    <Link href="/" className="flex items-center">
+      <div className="relative w-[48px] h-[48px] md:w-[72px] md:h-[72px]">
+        <Image 
+          src='/images/logo.png'
+          alt='EarlySteps Logo'
+          fill
+          sizes="(max-width: 768px) 48px, 72px"
+          className="object-contain"
+          priority
+        />
+      </div>
     </Link>
   );
 } 
